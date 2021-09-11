928bd1d26ed86f7adf8888bb84324b1f0d8bdd38
Thanatos095 <87665048+Thanatos095@users.noreply.github.com>
2021-09-11 12:01:47 +0500

    Add files via upload

---
+++ Layer.py
@@ -0,0 +1,10 @@
+class Layer:
+    def __init__(self):
+        self.input = None
+        self.trainable = True
+
+    def forward(self, input):
+        pass
+
+    def backward(self, output):
+        pass
